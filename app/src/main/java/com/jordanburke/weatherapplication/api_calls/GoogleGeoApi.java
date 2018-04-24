62e56cf879c82198d070bda82c354c237c5d33cb
JordanRBurke <jordanrobertburke@gmail.com>
2018-04-24 16:33:51 -0400

    Added new interface for api and also made fragment for weather information.

---
+++ app/src/main/java/com/jordanburke/weatherapplication/api_calls/GoogleGeoApi.java
@@ -0,0 +1,10 @@
+package com.jordanburke.weatherapplication.api_calls;
+
+import butterknife.BindView;
+import retrofit2.http.GET;
+import retrofit2.http.HTTP;
+
+public interface GoogleGeoApi {
+
+
+}
